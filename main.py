de0d27094edfae4c939a8616262ae124705b2c14
Ameno-ab <99277062+Ameno-ab@users.noreply.github.com>
2023-07-27 21:24:04 +0300

    wordnet_added

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from etnltk.tokenize.am import word_tokenize
 from etnltk.lang.am import normalize, remove_stopwords
 from etnltk.lang.am import preprocessing
 from etnltk.lang.am import clean_amharic
-from dataset import STOP_WORDS
+from stopwords_dataset import STOP_WORDS
+from wordnet import *
 
 
 
 def text_preprocessing(qnie):
-    cleaned = clean_amharic(qnie, abbrev=False)
-    print('cleaned',cleaned)
-    normalized_text = normalize(cleaned)
+    str1 = " "
+    normalized_text = normalize(qnie)
     print('text_normilized', normalized_text)
+    words = word_tokenize(normalized_text)
+    print('words', words)
+    cleaned = words
+    print('cleaned', cleaned)
     stop_word_remover(cleaned)
 
 
@@ -20,16 +24,21 @@ def text_preprocessing(qnie):
 def stop_word_remover(strings_set):
     words =list()
     str1 = " "
-    for string in strings_set.split():
+    for string in strings_set:
      if string not in STOP_WORDS:
          words.append(string)
-    stop_word_string=str1.join(words)
-    words = word_tokenize(stop_word_string)
-    print('word_tokinize',words)
+    stop_word_string=words
+    print('word_tokinize',stop_word_string)
 
 
 
 if __name__ == '__main__':
-    text_preprocessing("ወደ አዳባባይ ወጥተህ፣ ከባለጋራህ ተሟግተህ፣ ክርክር ገጥመህ ወደ ማታ፣ለማፈር ነው ስትረታ")
+    text_preprocessing("ያችማ የኔ እት ቤት ሠርታ ነበረች፣ ሳትቀመጥበት ላፈርሰው ነው አለች።")
+    for key in noun:
+        for word in noun[key]:
+            print("noun_key",key, '->', word)
+    for key in verb:
+        for word in verb[key]:
+            print("verb_key", key, '->', word)
 
 
---
+++ wordnet.py
@@ -0,0 +1,25 @@
+noun = {
+    'synset_1': ['አበበ', 'አበባ', 'ከበደ'],
+    'synset_2': ['አበባ', 'እጸዋት', 'ችግይ'],
+    # 'synset_3': ['dog', 'cat', 'fish']
+}
+# synset_1 related words with human names
+# synset_2 words with name of flowers
+
+verb = {
+    'synset_1': ['አበበ', 'ለማ', 'ፈካ'],
+    'synset_2': ['መበላት', 'መዋት', 'ማየክ'],
+    'synset_3': ['መቾሆክ', 'ደምጽ ማሰማት']
+}
+# synset_1 related words  for flourish
+# synset_2 related words  for eating
+# synset_3 related words  for shouting
+
+
+#for example:
+# abebab can have two noun meanings in noun synset_1 and synset_2
+# abebe will have two meaning one in noun synset_1 and in verb sysnset_1
+
+# so since abeba has two noun menaing it will have high chance of being hebir kal
+
+#so this is how we do the wordnet......
