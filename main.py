e9ceb3572e02a572985e1cfdf190a678996ea1fb
Ameno-ab <99277062+Ameno-ab@users.noreply.github.com>
2023-07-28 19:17:00 +0300

    double_meaning_identifier_added

--- main.py
+++ main.py
@@ -28,17 +28,69 @@ def stop_word_remover(strings_set):
      if string not in STOP_WORDS:
          words.append(string)
     stop_word_string=words
-    print('word_tokinize',stop_word_string)
+    double_meaning_identifier(stop_word_string)
+
+def double_meaning_identifier(words):
+
+# to identify words with double noun meanings
+  double=list()
+  noun_dict = {}
+  for word in words:
+      n = 0
+      for key in noun:
+         for noun_word in noun[key]:
+            if noun_word == word:
+                 n += 1
+                 noun_dict[word]= ['NN',n]
+  print("noun_key->", noun_dict)
+
+# to identify words with double verb meanings
+  verb_dict = {}
+  for word in words:
+      v = 0
+      for key in verb:
+          for verb_word in verb[key]:
+              if verb_word == word:
+                  v += 1
+                  verb_dict[word] = ['VV', v]
+  print("Verb_key->", verb_dict)
+
+# to identify words with double noun and verb meanings
+  noun_verb_dict = {}
+  for n_key  in noun_dict:
+      for v_key in verb_dict:
+          # print("key", v_key)
+          if v_key == n_key:
+           noun_verb_dict[n_key] = ['NV', noun_dict[n_key][-1] + verb_dict[v_key][-1]]
+  print("noun_verb_key", noun_verb_dict)
+
+#to merge all the identified meanings
+  merged={}
+  merged.update(noun_dict)
+  merged.update(verb_dict)
+  merged.update(noun_verb_dict)
+  print('merged',merged)
+
+#to set hebir kal with biggest counter number and tagger
+  hebir_kal=''
+  nn = False
+  for L_counter in  merged:
+   if merged[L_counter][0] == 'NV' and merged[L_counter][-1] > 1 and nn != True :
+        hebir_kal = L_counter
+   elif merged[L_counter][0] == 'VV' and  merged[L_counter][-1] > 1 :
+        hebir_kal = L_counter
+        break
+   elif merged[L_counter][0] == 'NN' and merged[L_counter][-1] > 1:
+        hebir_kal = L_counter
+        nn=True
+
+  print("hebitr kal",hebir_kal)
+  return hebir_kal
 
 
 
 if __name__ == '__main__':
-    text_preprocessing("ያችማ የኔ እት ቤት ሠርታ ነበረች፣ ሳትቀመጥበት ላፈርሰው ነው አለች።")
-    for key in noun:
-        for word in noun[key]:
-            print("noun_key",key, '->', word)
-    for key in verb:
-        for word in verb[key]:
-            print("verb_key", key, '->', word)
+   text_preprocessing(" በላ ለማ አበበ አበባ  ")
+
 
 
--- wordnet.py
+++ wordnet.py
@@ -1,15 +1,15 @@
 noun = {
-    'synset_1': ['አበበ', 'አበባ', 'ከበደ'],
-    'synset_2': ['አበባ', 'እጸዋት', 'ችግይ'],
-    # 'synset_3': ['dog', 'cat', 'fish']
+    'synset_1': ['አበበ', 'አበባ', 'ከበደ','ለማ'],
+    'synset_2': ['አበባ', 'እፀዋት', 'ችግይ'],
+    # 'synset_3': ['በላ']
 }
 # synset_1 related words with human names
 # synset_2 words with name of flowers
 
 verb = {
     'synset_1': ['አበበ', 'ለማ', 'ፈካ'],
-    'synset_2': ['መበላት', 'መዋት', 'ማየክ'],
-    'synset_3': ['መቾሆክ', 'ደምጽ ማሰማት']
+    'synset_2': ['በላ', 'መዋት', 'ማየክ'],
+    'synset_3': ['መቾሆክ', 'ደምጽ ማሰማት','በላ']
 }
 # synset_1 related words  for flourish
 # synset_2 related words  for eating
