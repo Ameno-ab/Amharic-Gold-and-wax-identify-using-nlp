104df710b8ca1f5ea965af3c597d6c55be3dd0e0
Ameno-ab <99277062+Ameno-ab@users.noreply.github.com>
2023-07-31 17:20:35 +0300

    adjective_and_adverb_added

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ def stop_word_remover(strings_set):
      if string not in STOP_WORDS:
          words.append(string)
     stop_word_string=words
+    print("stop words",stop_word_string)
     double_meaning_identifier(stop_word_string)
 
 def double_meaning_identifier(words):
-
-# to identify words with double noun meanings
+#
+# # to identify words with double noun meanings
   double=list()
   noun_dict = {}
   for word in words:
@@ -44,7 +45,7 @@ def double_meaning_identifier(words):
                  noun_dict[word]= ['NN',n]
   print("noun_key->", noun_dict)
 
-# to identify words with double verb meanings
+ # to identify words with double verb meanings
   verb_dict = {}
   for word in words:
       v = 0
@@ -64,25 +65,135 @@ def double_meaning_identifier(words):
            noun_verb_dict[n_key] = ['NV', noun_dict[n_key][-1] + verb_dict[v_key][-1]]
   print("noun_verb_key", noun_verb_dict)
 
-#to merge all the identified meanings
+
+# to identify words with double adjective meanings
+  adjective_dict = {}
+  for word in words:
+    adj = 0
+    for key in adjective:
+        for adjective_word in adjective[key]:
+            if adjective_word == word:
+                adj += 1
+                adjective_dict[word] = ['Adj', adj]
+  print("adjective_word->", adjective_dict)
+
+
+# to identify words with double noun and adjective meanings
+  noun_adjective_dict = {}
+  for n_key  in noun_dict:
+      for adj_key in adjective_dict:
+          # print("key", v_key)
+          if adj_key == n_key:
+           noun_adjective_dict[n_key] = ['AdjN', noun_dict[n_key][-1] + adjective_dict[adj_key][-1]]
+  print("noun_adjective_key", noun_adjective_dict)
+
+# to identify words with double verb and adjective meanings
+  verb_adjective_dict = {}
+  for v_key  in verb_dict:
+      for adj_key in adjective_dict:
+          # print("key", v_key)
+          if adj_key == v_key:
+           verb_adjective_dict[v_key] = ['AdjV', verb_dict[v_key][-1] + adjective_dict[adj_key][-1]]
+  print("verb_adjective_key", verb_adjective_dict)
+
+# to identify words with double adverb meanings
+  adverb_dict = {}
+  for word in words:
+    adv= 0
+    for key in adverb:
+        for adverb_word in adverb[key]:
+            if adverb_word == word:
+                adv += 1
+                adverb_dict[word] = ['Adv', adv]
+  print("adverb_word->", adverb_dict)
+
+# to identify words with double noun and adverb meanings
+  noun_adverb_dict = {}
+  for n_key  in noun_dict:
+      for adv_key in adverb_dict:
+          # print("key", v_key)
+          if adv_key == n_key:
+           noun_adverb_dict[n_key] = ['AdvN', noun_dict[n_key][-1] + adverb_dict[adv_key][-1]]
+  print("noun_adverb_key", noun_adverb_dict)
+
+# to identify words with double verb and adverb meanings
+  verb_adverb_dict = {}
+  for v_key  in verb_dict:
+      for adv_key in adverb_dict:
+          # print("key", v_key)
+          if adv_key == v_key:
+           verb_adverb_dict[v_key] = ['AdvV', verb_dict[v_key][-1] + adverb_dict[adv_key][-1]]
+  print("verb_adverb_key", verb_adverb_dict)
+
+
+# to identify words with double adjective and adverb meanings
+  adjective_adverb_dict = {}
+  for adj_key  in adjective_dict:
+      for adv_key in adverb_dict:
+          # print("key", v_key)
+          if adv_key == adj_key:
+           adjective_adverb_dict[adj_key] = ['AdvAdj', adjective_dict[adj_key][-1] + adverb_dict[adv_key][-1]]
+  print("adjective_adverb_key", adjective_adverb_dict)
+
+
+# to merge all the identified meanings
   merged={}
   merged.update(noun_dict)
   merged.update(verb_dict)
   merged.update(noun_verb_dict)
+  merged.update(adjective_dict)
+  merged.update(noun_adjective_dict)
+  merged.update(verb_adjective_dict)
+  merged.update(adverb_dict)
+  merged.update(noun_adverb_dict)
+  merged.update(verb_adverb_dict)
+  merged.update(adjective_adverb_dict)
   print('merged',merged)
 
 #to set hebir kal with biggest counter number and tagger
   hebir_kal=''
   nn = False
+  nv = False
+  adj = False
+  adv = False
+  adjV = False
+  adjN = False
+  advV = False
+  advN = False
+
+
   for L_counter in  merged:
    if merged[L_counter][0] == 'NV' and merged[L_counter][-1] > 1 and nn != True :
         hebir_kal = L_counter
+        nv=True
    elif merged[L_counter][0] == 'VV' and  merged[L_counter][-1] > 1 :
         hebir_kal = L_counter
         break
    elif merged[L_counter][0] == 'NN' and merged[L_counter][-1] > 1:
         hebir_kal = L_counter
         nn=True
+   elif merged[L_counter][0] == 'Adj' and merged[L_counter][-1] > 1 and nn != True and nv != True:
+        hebir_kal = L_counter
+        adj=True
+   elif merged[L_counter][0] == 'Adv' and merged[L_counter][-1] > 1 and nn != True and nv != True and adj != True:
+        hebir_kal = L_counter
+        adv = True
+   elif merged[L_counter][0] == 'AdjV' and merged[L_counter][-1] > 1 and nn != True and nv != True and adj != True and adv != True :
+        hebir_kal = L_counter
+        adjV = True
+   elif merged[L_counter][0] == 'AdjN' and merged[L_counter][-1] > 1 and nn != True and nv != True and adj != True and adv != True and adjV != True:
+       hebir_kal = L_counter
+       adjN = True
+   elif merged[L_counter][0] == 'AdvV' and merged[L_counter][-1] > 1 and nn != True and nv != True and adj != True and adv != True and adjV != True and adjN  != True:
+       hebir_kal = L_counter
+       advV = True
+   elif merged[L_counter][0] == 'AdvN' and merged[L_counter][-1] > 1 and nn != True and nv != True and adj != True and adv != True and adjV != True and adjN != True and advV != True:
+       hebir_kal = L_counter
+       advN = True
+   elif merged[L_counter][0] == 'AdvAdj' and merged[L_counter][-1] > 1 and nn != True and nv != True and adj != True and adv != True and adjV != True and adjN != True and advV != True and advN != True:
+       hebir_kal = L_counter
+
+
 
   print("hebitr kal",hebir_kal)
   return hebir_kal
@@ -90,7 +201,18 @@ def double_meaning_identifier(words):
 
 
 if __name__ == '__main__':
-   text_preprocessing(" በላ ለማ አበበ አበባ  ")
+   text_preprocessing("ያቶ ሰይፉ ባሕርይ እጅግ ያስቸግራል፣ ጠላቱን እጅ ነሥቶ ለወዳጁ ይኮራል።")
 
 
 
+
+#  "አምላክ ግብር ጠርቶ ያበላል ይላሉ፤ አንድም ቀሪ የለም ሁሉም ይጠራሉ።"(ይጠራሉ)
+# "አራሲቱ ወተት ጠጪ ይሻልሻል፣ አልቦ ይዞ ባልሽ ያስፈርድብሻል፡፡"አልቦ:(ከወርቅና ከብር በዶቃ አምሳል የተሠራ የሴቶች እግር ጌጥ)
+#  "አርባ ሑዳዴን መሖደድ፣ አክፍለት አለ የግድ፣ የሰው ሁሉ መጎጃው፣ በማማት እንጂ ነው፡፡"(በማማት)
+# "አምናና ካችአምና ደኀና ሰው ነበረች፣ በሽታዋ መጣ ትተኩስ ጀመረች፡፡"(ትተኩስ)
+#  "አሳላፊው ሁሉ ባይሆነን ወዳጅ፣ ሳያድለን ቀረ እኛን ጠላ ጠጅ፡፡"(ሳያድለን)
+# "አሳብም የለብኝ እኔስ ዐርፌያለሁ፣ በቆላም በደጋም ዘሬን ጨርሻለሁ፡፡"(ዘሬን)
+# "አሽከሬ ጠፍቶኛል በቅሎዬም ጠፍቶኛል፣ አፋልገኝ ጎጃሜ በባል ቀን ይገኛል፡፡"(በባል)
+# "እናትና አባትሽ ሥጋ ነው እርማቸው፣ አንዠት ትበያለሽ አንቺማ ልጃቸው"(አንዠት)
+# "እናት አባት ንቃ ወጥታ ከፈቃድ፣ እዩዋት ይችህን ወጣት ባልጋ ስትሄድ፡፡"(ባልጋ)
+# "ሳላይ ነው እንጂ በድንገት፣ ሚስቴን ሳይ በወሰዳት"(ሳይ)
--- wordnet.py
+++ wordnet.py
@@ -1,7 +1,7 @@
 noun = {
-    'synset_1': ['አበበ', 'አበባ', 'ከበደ','ለማ'],
+    'synset_1': ['አበበ', 'አበባ', 'ከበደ', 'ለማ', 'ሰይፉ'],
     'synset_2': ['አበባ', 'እፀዋት', 'ችግይ'],
-    # 'synset_3': ['በላ']
+
 }
 # synset_1 related words with human names
 # synset_2 words with name of flowers
@@ -9,17 +9,36 @@ noun = {
 verb = {
     'synset_1': ['አበበ', 'ለማ', 'ፈካ'],
     'synset_2': ['በላ', 'መዋት', 'ማየክ'],
-    'synset_3': ['መቾሆክ', 'ደምጽ ማሰማት','በላ']
+    'synset_3': ['መቾሆክ', 'ደምጽ ማሰማት', 'በላ'],
+    'synset_4': ['ነስቶ', 'ፊት መንሳት', 'ማገለል'],
+    'synset_5': ['ማከበር', 'ዘክ ማለት'],
+
+
+}
+
+adjective = {
+
+    'synset_2': ['ባህርይ', 'ጸባይ'],
+    'synset_3': ['ያስቸግራል', 'የረብሻል'],
+
+
+
+}
+
+adverb = {
+    'synset_1': ['እጅግ', 'በጣም'],
+    'synset_3': ['ያስቸግራል', 'የረብሻል'],
+
 }
 # synset_1 related words  for flourish
 # synset_2 related words  for eating
 # synset_3 related words  for shouting
 
 
-#for example:
+# for example:
 # abebab can have two noun meanings in noun synset_1 and synset_2
 # abebe will have two meaning one in noun synset_1 and in verb sysnset_1
 
 # so since abeba has two noun menaing it will have high chance of being hebir kal
 
-#so this is how we do the wordnet......
+# so this is how we do the wordnet......
